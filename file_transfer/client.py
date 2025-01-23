21ce0ad84fe22af0ed9f80edaeae638c36f3af0f
Larissa Samara <larissasamaraxavier@gmail.com>
2025-01-23 18:57:28 -0300

    cliente grpc em python

---
+++ file_transfer/client.py
@@ -0,0 +1,20 @@
+import grpc
+import file_transfer_pb2
+import file_transfer_pb2_grpc
+
+def upload_file(stub, file_path):
+    with open(file_path, 'rb') as f:
+        content = f.read()
+        filename = file_path.split('/')[-1]
+        request = file_transfer_pb2.FileRequest(filename=filename, content=content)
+        response = stub.UploadFile(request)
+        print(response.message)
+
+def main():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = file_transfer_pb2_grpc.FileTransferStub(channel)
+    file_path = input("Enter the .txt file to upload: ")
+    upload_file(stub, f"files/{file_path}")
+
+if __name__ == "__main__":
+    main()
